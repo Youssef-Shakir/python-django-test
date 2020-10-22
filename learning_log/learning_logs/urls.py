29c7eedeff7509ed95925d70a8cbc1cfd016b7e4
youssef shakir <youssef.shakir.2003@gmail.com>
2020-10-22 15:17:02 +0300

    laptop

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'learning_log'
 urlpatterns = [
 	path('',views.index,name='index'),
+	path('topics/',views.topics,name='topics'),
 ]
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import topic
 # Create your views here.
-def index(requst):
-	return render(requst,'learning_logs/index.html')
+def index(request):
+	return render(request,'learning_logs/index.html')
+
+def topics(request):
+	topics = topic.objects.order_by('date_added')
+	context = {'topics':topics}
+	return render(request , 'learning_logs/topics.html',context)
