76a77a61434733a261df3f171fb5ef2e35455a2f
youssef shakir <youssef.shakir.2003@gmail.com>
2020-10-24 16:33:20 +0300

    laptop

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,20 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """A topic the user is learning about."""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True) 
 
-class topic(models.Model):
-	text = models.CharField(max_length=200)
-	date_added = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """Return a string representation of the model."""
+        return self.text
 
-	def __str__(self):
-		return self.text
-
-		
-class entry(models.Model):
-	topic = models.ForeignKey(topic , on_delete=models.CASCADE)
-	text = models.TextField()
-
-class Meta:
-	verbose_name_plural = 'entry'
-	def __str__(self):
-		return f"{self.text[:50]}..."
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural = 'entries'
+ 
+    def __str__(self):
+        """Return a string representation of the model."""
+        return f"{self.text[:50]}..."
--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,10 +1,21 @@
+
 from django.shortcuts import render
-from .models import topic
-# Create your views here.
+
+from .models import Topic
+
 def index(request):
-	return render(request,'learning_logs/index.html')
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
 
 def topics(request):
-	topics = topic.objects.order_by('date_added')
-	context = {'topics':topics}
-	return render(request , 'learning_logs/topics.html',context)
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
