0470a410a682d82dcb7a14f64ff886bfc8176bae
youssef shakir <youssef.shakir.2003@gmail.com>
2020-10-25 14:53:49 +0300

    laptop

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 
 from .models import Topic
-
+from .forms import TopicForm
 def index(request):
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
@@ -19,3 +19,13 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+def new_topic(request):
+	if request.method != 'POST':
+		form = TopicForm()
+	else:
+		form = TopicForm(data=request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('learning_logs:topics')
+	context = {'form': form}
+	return render(request, 'learning_logs/new_topic.html',context)
